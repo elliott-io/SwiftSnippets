da7e4f45bb7c71773b9e3acc0db7d66507347a60
Elliott <meraki.club@gmail.com>
2020-04-27 11:51:12 -0700

    Missing Popover Messages snippet files that weren't automatically commited by Xcode.

---
+++ StoryboardSnippets/StoryboardSnippets/Snippets/PopoverMessage/AlwaysPresentAsPopover.swift
@@ -0,0 +1,31 @@
+//
+//  AlwaysPresentAsPopover.swift
+//  StoryboardSnippets
+//
+//  Created by Elliott Io on 4/26/20.
+//  Copyright © 2020 elliott io. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AlwaysPresentAsPopover : NSObject, UIPopoverPresentationControllerDelegate {
+    
+    // `sharedInstance` because the delegate property is weak - the delegate instance needs to be retained.
+    private static let sharedInstance = AlwaysPresentAsPopover()
+    
+    private override init() {
+        super.init()
+    }
+    
+    func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
+        return .none
+    }
+    
+    static func configurePresentation(forController controller : UIViewController) -> UIPopoverPresentationController {
+        controller.modalPresentationStyle = .popover
+        let presentationController = controller.presentationController as! UIPopoverPresentationController
+        presentationController.delegate = AlwaysPresentAsPopover.sharedInstance
+        return presentationController
+    }
+}
